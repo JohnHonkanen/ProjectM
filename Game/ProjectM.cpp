00ed5840ed558647812168d021a3d14b629923ea
John Honkanen <jc.honkanen@gmail.com>
2018-01-24 15:51:53 +0000

    Configured Project Paths

---
+++ Game/ProjectM.cpp
@@ -0,0 +1,11 @@
+
+#include <Engine\MobaJuiceCore.h>
+
+int main(int argc, char *argv[])
+{
+	/*GameEngine engine = GameEngine();
+	engine.LoadSettings("default-settings.xml");
+	Scene *scene = engine.CreateScene("BlankScene");
+	engine.Run();*/
+	return 0;
+}
