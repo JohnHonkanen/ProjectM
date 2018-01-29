ce349cf4c166c1f040357977b388d3786c1a88a8
John Honkanen <jc.honkanen@gmail.com>
2018-01-29 20:37:27 +0000

    Created Blank Scene

--- Game/ProjectM.cpp
+++ Game/ProjectM.cpp
@@ -1,11 +1,12 @@
 
-#include <Engine\MobaJuiceCore.h>
+#include "MobaJuiceCore.h"
 
 int main(int argc, char *argv[])
 {
-	/*GameEngine engine = GameEngine();
-	engine.LoadSettings("default-settings.xml");
+	GameEngine engine = GameEngine();
+	//engine.SetDefaultPath(GameEngine::Paths::SETTINGS, "../GameEngine/MobaJuiceEngine/Engine/settings/");
+	engine.LoadSettings(string(engine.GetPath(GameEngine::Paths::SETTINGS) +  "default-settings.xml").c_str());
 	Scene *scene = engine.CreateScene("BlankScene");
-	engine.Run();*/
+	engine.Run();
 	return 0;
 }
