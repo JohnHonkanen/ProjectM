f2a88e7305733c8dd2ade1e8ad258e5eab4a47a5
Lapalm <anthony_palm@hotmail.co.jp>
2018-02-15 19:26:18 +0000

    populated contract get/set :ers WIP

--- Game/Scripts/Contract.cpp
+++ Game/Scripts/Contract.cpp
@@ -4,17 +4,75 @@ Contract::Contract()
 {
 }
 
-Contract::Contract(Resources resource, int paymentToRecieve, int amountToFulfill, int contractTime, int currentFullFilled)
+Contract::Contract(Resources resource, int paymentToRecieve, int amountToFulfill, int contractTime, int currentFulFilled, int difficultyLevel)
 {
 	this->Resource = resource;
 	this->payment = paymentToRecieve;
 	this->amount = amountToFulfill;
 	this->time = contractTime;
-	this->current = currentFullFilled;
+	this->current = currentFulFilled;
+	this->difficulty = difficultyLevel;
 }
 
 Contract::~Contract()
 {
+}
+
+int Contract::GetDifficulty()
+{
+	return this->difficulty;
+}
+
+void Contract::SetDifficulty()
+{
+	this->difficulty = difficulty;
+}
+
+int Contract::GetPayment()
+{
+	return this->payment;
+}
+
+Resources Contract::GetResource()
+{
+	return this->Resource;
+}
+
+int Contract::GetAmount()
+{
+	return this->amount;
+}
+
+void Contract::SetTime()
+{
+	this->time = time;
+}
+
+int Contract::GetTime()
+{
+	return this->time;
+}
+
+
+int Contract::GetCurrent()
+{
+	// currently fulfilled
+	return this->current;
+}
+
+bool Contract::GetStatus()
+{
+	return this->active;
+}
+
+void Contract::SetStatus(bool active)
+{
+	this->active = true;
+}
+
+bool Contract::IsComplete()
+{
+	return this->complete;
 }
 
 Contract * Contract::Create(GameObject * gameObject)
--- Game/Scripts/Contract.h
+++ Game/Scripts/Contract.h
@@ -7,18 +7,33 @@
 class Contract : public Behaviour{
 public:
 	Contract();
-	Contract(Resources resource, int paymentToRecieve, int amountToFulfill, int contractTime, int currentFullFilled);
+	Contract(Resources resource, int paymentToRecieve, int amountToFulfill, int contractTime, int currentFulFilled, int difficultyLevel);
 	~Contract();
+
+	int GetDifficulty(); // Gets contract difficulty
+	void SetDifficulty(); // Sets contract difficulty, default is 1.
+	int GetPayment(); // Get payment amount player is to receive if contract is fulfilled.
+	Resources GetResource(); 
+	int GetAmount(); // Get amount to fulfill on contract
+	void SetTime(); // Set time left on contract
+	int GetTime(); // Get time left on contract
+	int GetCurrent(); // Get current amount fulfilled of contract
+
+	bool GetStatus(); // Get the status of contract (true/false)
+	void SetStatus(bool active); // Return true
+	
+	bool IsComplete(); // Return true
+
 
 	static Contract* Create(GameObject *gameObject);
 	void Copy(GameObject *copyObject);
 	void Update();
-
 	void Start();
 
 private:
 	int payment, amount, time;
 	int current = 0;
+	int difficulty = 1;
 	Resources Resource;
 
 	bool active = false;
