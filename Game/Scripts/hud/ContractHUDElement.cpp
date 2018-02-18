20f476e4100fa4834b1546b963ec657df1d219a7
Lapalm <anthony_palm@hotmail.co.jp>
2018-02-18 19:44:14 +0000

    updated HUD so it displays one contract

--- Game/Scripts/hud/ContractHUDElement.cpp
+++ Game/Scripts/hud/ContractHUDElement.cpp
@@ -19,8 +19,30 @@ void ContractHUDElement::Start()
 {
 	// Text
 	contractHUD = EHUD::WHUDContainer::Create(this, { 0, 0, 250, 150 }, "Game/Assets/Textures/black.jpg", true);
-	text = EHUD::TextWidget::Create(contractHUD, { 10, 25, 100, 100 }, " 123", "Game/Assets/Fonts/MavenPro-Regular.ttf", 16, 1, vec3(1, 1, 1));
 
+	// Contract Issue Number
+	EHUD::TextWidget::Create(contractHUD, { 10, 16, 0, 0 }, "Contract Issue Number: " + to_string(contract->GetContractIndex()), "Game/Assets/Fonts/MavenPro-Regular.ttf", 16, 1, vec3(1, 1, 1));
+
+	// Contract ID
+	EHUD::TextWidget::Create(contractHUD, { 10, 32, 0, 0 }, "Contract ID: " + to_string(contract->GetContractID()), "Game/Assets/Fonts/MavenPro-Regular.ttf", 16, 1, vec3(1, 1, 1));
+
+	// Contract Difficulty
+	EHUD::TextWidget::Create(contractHUD, { 10, 48, 0, 0 }, "Difficulty Level: " + to_string(contract->GetDifficulty()), "Game/Assets/Fonts/MavenPro-Regular.ttf", 16, 1, vec3(1, 1, 1));
+
+	// Contract Amount to FulFill
+	EHUD::TextWidget::Create(contractHUD, { 10, 64, 0, 0 }, "Amount to FulFill: " + to_string(contract->GetCurrent()) + "/" + to_string(contract->GetAmount()), "Game/Assets/Fonts/MavenPro-Regular.ttf", 16, 1, vec3(1, 1, 1));
+
+	// Contract Reward
+	EHUD::TextWidget::Create(contractHUD, { 10, 80, 0, 0 }, "Reward: " + to_string(contract->GetPayment()), "Game/Assets/Fonts/MavenPro-Regular.ttf", 16, 1, vec3(1, 1, 1));
+
+	// Contract Time Left
+	EHUD::TextWidget::Create(contractHUD, { 10, 96, 0, 0 }, "Resource to Deliver: " + to_string(contract->GetResourceID()), "Game/Assets/Fonts/MavenPro-Regular.ttf", 16, 1, vec3(1, 1, 1));
+
+	// Contract Status (Complete/!Complete)
+	EHUD::TextWidget::Create(contractHUD, { 10, 112, 0, 0 }, "Status (Active/!Active): " + to_string(contract->GetStatus()), "Game/Assets/Fonts/MavenPro-Regular.ttf", 16, 1, vec3(1, 1, 1));
+
+	text = EHUD::TextWidget::Create(contractHUD, { 10, 128, 0, 0 }, " ", "Game/Assets/Fonts/MavenPro-Regular.ttf", 16, 1, vec3(1, 1, 1));
+	
 	StartChildWidgets();
 }
 
@@ -31,6 +53,6 @@ void ContractHUDElement::Update()
 
 void ContractHUDElement::DrawWidget(unsigned int shader)
 {
-	text->text = to_string((int)contract->GetTime() / 1000) + "s";
+	text->text = "Time Left: " + to_string((int)contract->GetTime() / 1000) + "s";
 	cout << text->text << endl << endl;
 }
