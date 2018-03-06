e6787cd75a697e3b624ed0ee0bebd8aadcfc45c6
John Honkanen <jc.honkanen@gmail.com>
2018-03-06 17:20:36 +0000

    Replaced BuildingListButton with Generic class

--- Game/Scripts/hud/MainHUD.cpp
+++ Game/Scripts/hud/MainHUD.cpp
@@ -3,7 +3,7 @@
 #include "hud\HUDCanvas.h"
 #include "hud\widgets\HUDContainer.h"
 #include "hud\widgets\TextWidget.h"
-#include "BuildingListButton.h"
+#include "WidgetToggleButton.h"
 #include "BuildingListGroupElement.h"
 MainHUD * MainHUD::Create(GameObject * gameObject, EHUD::HUDCanvas * root)
 {
@@ -24,7 +24,7 @@ void MainHUD::OnLoad()
 {
 	wrapper = EHUD::WHUDContainer::Create(root, { 0 , 0, 1280, 720 }, "Game/Assets/Textures/size_test.png", true);
 
-	BuildingListButton::Create(wrapper, {35,500 , 60,60}, "Game/Assets/Textures/circle.png",
+	WidgetToggleButton::Create(wrapper, {35,500 , 60,60}, "Game/Assets/Textures/circle.png",
 		BuildingListGroup::Create(wrapper, { 105, 460, 0, 0 })
 		);
 	
--- Game/Scripts/hud/WidgetToggleButton.h
+++ Game/Scripts/hud/WidgetToggleButton.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "AnimatedButton.h"
 
-class BuildingListButton : public AnimatedButton
+class WidgetToggleButton : public AnimatedButton
 {
 public:
-	static BuildingListButton * Create(HUDElement * element, EHUD::HUDRect rect, std::string baseTexture, HUDElement *group);
+	static WidgetToggleButton * Create(HUDElement * element, EHUD::HUDRect rect, std::string baseTexture, HUDElement *group);
 private:
 	/** Pointer Click Event Implementation */
 	virtual void OnPointerMouseDownImplementation(EventData data);
