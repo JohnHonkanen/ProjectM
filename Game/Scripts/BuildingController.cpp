08ebf5e21a3c093d983404c2c216b95d1a794f54
Lapalm <anthony_palm@hotmail.co.jp>
2018-04-03 23:15:27 +0100

    Activated lights on buildings

--- Game/Scripts/BuildingController.cpp
+++ Game/Scripts/BuildingController.cpp
@@ -87,6 +87,7 @@ void BuildingController::Update(double dt)
 						{
 							GameObject * structure = buildingManager->GetBuilding(structureName);
 							Structure *sComponent = structure->GetComponent<Structure>();
+							structure->GetComponent<LightCycle>()->ActivateLight();
 							mouseHeld = true;
 							sComponent->SetTilePosition(coordinates.x, coordinates.y);
 							sComponent->SetActive(false); //Turn on structures
